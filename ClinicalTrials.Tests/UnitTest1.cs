c02b51272a864323a98fe96890d50a2b84a16b2c
Dejan Zivkovic <dejan.zivkovic90@gmail.com>
2024-12-20 10:53:09 +0100

    add unit tests projects

---
+++ ClinicalTrials.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using ClinicalTrialsApi.Controllers;
+
+namespace ClinicalTrials.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+        }
+    }
+}
